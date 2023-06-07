4eb8e10f2eca7d35d6d0d9f7197dc3c10e0b9668
Medrine-Jepkemoi <jepkemoimedrine@gmail.com>
2023-06-07 08:47:34 +0300

    admin methods

--- lib/admin.py
+++ lib/admin.py
@@ -1,4 +1,5 @@
-from models import Admin, session_maker
+from models import Admin, Product, session_maker
+from product import products
 
 admins =  Admin(admin_fname = "Medrine", admin_lname = "Jepkemoi"),
 
@@ -9,3 +10,45 @@ with session_maker() as session:
     session.commit()
 
 # function to check if admin is valid. There can only be one admin
+
+# Admin adding product amount
+def add_stock(product_id, quantity):
+    with session_maker() as session:
+        product = session.query(Product).get(product_id)
+        product.product_amount += quantity
+        session.commit()
+
+# add_stock(1, 2)
+
+# Admin updating product price
+def update_price(product_id, price):
+    with session_maker() as session:
+        product = session.query(Product).get(product_id)
+        product.product_price = price
+        session.commit()
+
+# update_price(1, 15)
+
+def add_product(name, description, price, amount, category, admin):
+    with session_maker() as session:
+        product = session.query(Product)
+        product = Product(product_name = name, product_description = description, product_price = price, product_amount = amount, category_id = category, admin_id = admin)
+        session.add(product)
+        session.commit()
+
+# add_product("Apple", "This is a fruit rich in iron", 30, 100, 1, 1)
+
+def view_products():
+    with session_maker() as session:
+        product = session.query(Product).all
+        for product in products:
+            print(product)
+        session.commit()
+# view_products()
+
+def delete_product(product_id):
+    with session_maker() as session:
+        product = session.query(Product).filter(Product.product_id == product_id).first()
+        session.delete(product)
+        session.commit()
+delete_product(2)
--- lib/customers.py
+++ lib/customers.py
@@ -1,4 +1,4 @@
-from models import Customer, faker, session_maker
+from models import Customer, OrderItem, Product, faker, session_maker
 
 customers = [
     Customer(customer_fname = faker.first_name(), customer_lname = faker.last_name(), customer_mobile = '0765890766'),
@@ -27,6 +27,53 @@ def read_customers():
 
 # read_customers()
 
+# Validate customer
+
+# def customer_login(self, customer_id):
+#     with session_maker() as session:
+#         customer = session.query(Customer).filter(Customer.customer_id == customer_id).all()
+#         for customer in customers:
+#             if customer in customers:
+#                 print("Customer exists")
+#             else:
+#                 print("Sign up")
+
+# # customer_login( 3)
+
+# # Customer ordering
+# def customer_order(self, product_id, quantity):
+#     session = session_maker()
+#     product = session.query(Product).filter_by(product_id=product_id).first()
+
+#     if product is None:
+#         session.close()
+#         return "Invalid product ID. Order placement failed."
+
+#     if product.product_amount < quantity:
+#         session.close()
+#         return "Insufficient product quantity. Order placement failed."
+
+#     total_price = product.product_price * quantity
+
+#     order_item = OrderItem(product_id=product_id, quantity=quantity, totalprice=total_price, customer_id=self.customer_id)
+#     session.add(order_item)
+#     session.commit()
+#     session.close()
+
+#     return "Order placed successfully."
+
+# # Usage example:
+# customer_id = 1  # Example customer ID
+# product_id = 1  # Example product ID
+# quantity = 2
+
+# with session_maker() as session:
+#     customer = session.query(Customer).filter_by(customer_id=customer_id).first()
+#     if customer is None:
+#         print("Invalid customer ID.")
+#     else:
+#         result = customer.customer_order(product_id, quantity)
+#         print(result)
 
 
 
